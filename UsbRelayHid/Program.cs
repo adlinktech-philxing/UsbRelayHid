8420148ff2d57cdbde39c5aa32b11060c15d4111
Phil.Xing <phil.xing@adlinktech.com>
2022-03-26 02:08:25 +0800

    Initial

---
+++ UsbRelayHid/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Linq;
+using UsbRelayNet.RelayLib;
+
+namespace UsbRelayHid
+{
+    internal class Program
+    {
+        private static readonly RelaysEnumerator _relaysEnumerator = new RelaysEnumerator();
+        private static Relay _selectedRelay=null;
+
+
+        private static void switchRelay(bool switching)
+        {
+            int channel=1; // 1~8, for channel 1 control only by now
+
+            var items = _relaysEnumerator.CollectInfo()
+                .Select(x => new RelayItem(x))
+                .ToArray();
+            if (items.Length > 0)
+            {
+                _selectedRelay = new Relay(items[0].RelayInfo); // 1 relay only
+                _selectedRelay.Open();
+                //var channelsStatus = _selectedRelay.ReadChannels();
+                _selectedRelay.WriteChannel(channel, switching);
+                _selectedRelay.Close(); 
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            if (args.Length > 0)
+            {
+                if (args[0].ToLower() == "on")
+                {
+                    switchRelay(true);
+                }
+                else
+                {
+                    switchRelay(false);
+                }
+            }
+        }
+    }
+}
---
+++ UsbRelayHid/RelayItem.cs
@@ -0,0 +1,26 @@
+﻿using UsbRelayNet.RelayLib;
+
+namespace UsbRelayHid
+{
+    public class RelayItem {
+        private readonly RelayInfo _relayInfo;
+
+        public RelayItem(RelayInfo relayInfo) {
+            this._relayInfo = relayInfo;
+        }
+
+        public RelayInfo RelayInfo {
+            get {
+                return this._relayInfo;
+            }
+        }
+
+
+        public override string ToString() {
+            return string.Format(
+                "#{0}  @ '{1}'",
+                this._relayInfo.Id,
+                this._relayInfo.HidInfo.Path);
+        }
+    }
+}
